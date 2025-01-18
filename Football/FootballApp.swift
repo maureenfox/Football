76a72f85216ae8e85751304b5a5bf57dfdce0101
Maureen Fox <maureenfox2022@gmail.com>
2025-01-18 14:37:42 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Maureen Fox on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
